fab8a6b584ba4d98af8b6b0f7919b70b0af69db4
Felix <61414068+felix-rock2020@users.noreply.github.com>
2020-10-29 21:07:49 +0100

    Update Lesson 01

--- Lesson - 01/server.js
+++ Lesson - 01/server.js
@@ -1,21 +1,12 @@
-async function foo() {
-	console.log('foo Start');
-	let i = 0;
-	for (let index = 0; index < 1000; index++) {
-		i++
-	}
-	console.log('foo End');
+async function asyncCall() {
+	console.log('calling');
+	const result = await new Promise(resolve => {
+		setTimeout(() => {
+			resolve('resolved');
+		}, 2000);
+	});
+	console.log(result);
 }
 
-async function bar() {
-	console.log('bar Start');
-	let i = 0;
-	for (let index = 0; index < 1000; index++) {
-		i++;	
-	}
-	console.log('bar End');
-}
-
-foo();
-console.log("Server");
-bar();
+asyncCall();
+console.log("I am after asyncCall")
