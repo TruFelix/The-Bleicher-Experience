6b5c246352f0f05435b4c79911e632c204540f88
Felix <61414068+felix-rock2020@users.noreply.github.com>
2020-11-03 17:07:41 +0100

    Removed Lesson 05

--- Lesson - 05 - TypeScript/backend/out/server.js
+++
@@ -1,9 +0,0 @@
-// This code will be converted to JS
-// this code will be executed on the server and remains there.
-import express from 'express';
-const app = express();
-app.get('/', (req, res) => {
-    res.send("Hello World");
-});
-app.get('/*', (req, res) => {
-});
