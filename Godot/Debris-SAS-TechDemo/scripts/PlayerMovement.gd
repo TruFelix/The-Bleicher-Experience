47026d779d61e6b5750994b75b22288c5fe519ac
Felix Trutschnig <felix.trutschnig@kickscale.com>
2021-07-29 13:52:41 +0200

    2Modules Hooked Together

--- Godot/Debris-SAS-TechDemo/scripts/PlayerMovement.gd
+++ Godot/Debris-SAS-TechDemo/scripts/PlayerMovement.gd
@@ -7,7 +7,7 @@ const Telemetry = preload("./defs/Telemetry.gd")
 export (NodePath) var pathToRigidbody
 export (NodePath) var pathToModulesRoot
 export (float) var thrust = 10
-export (float) var angularThrust = 1
+export (float) var angularThrust = 50
 export (float) var sasStrength = 0.01
 
 var rigidbody : RigidBody2D
@@ -19,24 +19,26 @@ signal telemetry(delta, telemetry)
 
 # State
 var sasEnabled: bool = false
-
+var rb : RigidBody2D
 func _enter_tree():
 	rigidbody = get_node(pathToRigidbody) as RigidBody2D
-	
+
 	var modulesRoot: Node2D = get_node(pathToModulesRoot) as Node2D
 	var modules: Array = modulesRoot.get_children() as Array
 	for module in modules:
-		var rb : RigidBody2D = module as RigidBody2D
-		rb.mode = RigidBody2D.MODE_STATIC
+		rb = module as RigidBody2D
+#		rb.mode = RigidBody2D.MODE_KINEMATIC
+#		rb.apply_central_impulse(Vector2.ZERO)
 		rigidbody.mass+= rb.mass
 
 func _physics_process(delta):
 	var movement = handleLinear(delta)
 	var rotation = handleAngular(delta)
 	handleSas(delta, movement, rotation)
-	
+
 	act(movement, rotation)
-	
+#	rb.apply_central_impulse(movement * thrust)
+
 	# tell everyone all actions
 	emit_signal("rotate", delta, rotation)
 	emit_signal("move", delta, movement)
@@ -55,8 +57,8 @@ func act(move: Vector2, rotate: float):
 	move.x = clamp(move.x, -1, 1)
 	move.y = clamp(move.y, -1, 1)
 	rotate = clamp(rotate, -1, 1)
-	
-	
+
+
 	rigidbody.apply_torque_impulse(rotate * angularThrust)
 	rigidbody.apply_central_impulse(move.rotated(rigidbody.rotation) * thrust)
 
@@ -65,11 +67,11 @@ func handleLinear(delta) -> Vector2:
 	inputVector.x = Input.get_action_strength("go_right") - Input.get_action_strength("go_left")
 	inputVector.y = Input.get_action_strength("go_down") - Input.get_action_strength("go_up")
 	return inputVector * delta
-	
+
 func handleAngular(delta) -> float:
 	var rotation = Input.get_action_strength("rotate_right") - Input.get_action_strength("rotate_left")
 	return rotation * delta
-	
+
 func handleSas(delta: float, inputVector, inputRotate):
 	if Input.is_action_just_pressed("sas"):
 		sasEnabled = !sasEnabled
@@ -87,14 +89,14 @@ func angular_sas(delta: float, enabled: bool, inputRotate: float):
 		# default sasImpulse
 		var currentAngularSpeed = rigidbody.angular_velocity
 		var sasAngularImpulse : float = rigidbody.angular_velocity * -1 * sasStrength
-		
+
 		if currentAngularSpeed < 10 and currentAngularSpeed > 1:
 			sasAngularImpulse = clamp(rigidbody.angular_velocity * -1, -1, 1)
 		elif currentAngularSpeed <= 1:
 			sasAngularImpulse = rigidbody.angular_velocity * -1
-			
+
 		sasAngularImpulse *= delta
-		
+
 		emit_signal("rotate", delta, sasAngularImpulse)
 		act(Vector2.ZERO, sasAngularImpulse)
 
@@ -103,25 +105,25 @@ func linear_sas(delta: float, enabled: bool, inputVector: Vector2):
 		# default sasImpulse
 		var currentSpeed = rigidbody.linear_velocity.length()
 		var sasImpulse : Vector2 = rigidbody.linear_velocity.rotated(rigidbody.rotation * -1) * -1 * sasStrength
-		
+
 		# no sas if there's input
 		# otherwise max speed is limited by sas
 		if inputVector.x != 0:
 			sasImpulse.x = 0
-			
+
 		if inputVector.y != 0:
 			sasImpulse.y = 0
-		
+
 		# no further calculations needed
 		if sasImpulse.length() == 0:
 			return
-			
+
 		if currentSpeed < 10 and currentSpeed > 0.1:
 			sasImpulse = rigidbody.linear_velocity.normalized().rotated(rigidbody.rotation * -1) * (sasStrength * 10) * -1
 		elif currentSpeed <= 0.1:
 			sasImpulse = rigidbody.linear_velocity.rotated(rigidbody.rotation * -1) * -1
-		
+
 		sasImpulse *= delta
-		
+
 		emit_signal("move", delta, sasImpulse)
 		act(sasImpulse, 0)
